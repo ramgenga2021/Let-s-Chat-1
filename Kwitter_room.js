06ee2ff94d3dcc89c50b46d72d00ac5afa8964e8
ramgenga2021 <80458652+ramgenga2021@users.noreply.github.com>
2021-04-15 09:44:31 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,27 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
